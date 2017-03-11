2f19755914ee517345c5feda02b8e71aee856059
Drake Scott <dascott0204@gmail.com>
2017-03-10 22:55:19 -0600

    creating classes and class properties

--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
 /**
  * Created by dasco on 3/9/2017.
  */
+class Person {
+    name: string;
+    private type: string;
+    protected age: number;
+
+    constructor(name: string, public username: string) {
+        this.name = name;
+    }
+}
+const person = new Person("Drake", "Inuyasha5689");
+console.log(person.name, person.username);
