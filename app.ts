8394dfd8a4f77f29dc9f0a028c81d55948510916
Drake Scott <dascott0204@gmail.com>
2017-03-10 23:02:34 -0600

    class methods and access modifiers

--- app.ts
+++ app.ts
@@ -4,11 +4,23 @@
 class Person {
     name: string;
     private type: string;
-    protected age: number;
+    protected age: number = 20;
 
     constructor(name: string, public username: string) {
         this.name = name;
     }
+
+    printAge(){
+        console.log(this.age);
+        this.setType("old guy");
+    }
+
+    private setType(type: string){
+        this.type = type;
+        console.log(this.type);
+    }
 }
 const person = new Person("Drake", "Inuyasha5689");
 console.log(person.name, person.username);
+person.printAge();
+// person.setType("Cool guy"); wont work with private method
